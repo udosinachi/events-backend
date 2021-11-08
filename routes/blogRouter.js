bb94f1febe05089aa3f35ac7233208d6e836c326
Udosinachi <62754095+udosinachi@users.noreply.github.com>
2021-11-08 22:48:16 +0100

    Blog route error

--- routes/blogRouter.js
+++ routes/blogRouter.js
@@ -8,8 +8,8 @@ const {
   getBlogPostById,
 } = require('../controllers/blogController')
 
+router.route('/blogposts').get(getAllBlogPost)
 router.route('/blogpost').post(protect, createBlogPost)
 router.route('/:id').get(getBlogPostById)
-router.route('/blogposts').get(getAllBlogPost)
 
 module.exports = router
