587e409f32e6e118592d2c39a6b2ff99d1bbc680
Udosinachi <62754095+udosinachi@users.noreply.github.com>
2021-12-02 09:38:40 +0100

    Protect middleware added

--- routes/blogRouter.js
+++ routes/blogRouter.js
@@ -13,9 +13,9 @@ const {
 
 router.route('/blogposts').get(getAllBlogPost)
 router.route('/:id').get(getBlogPostById)
-router.route('/user/:id').get(getUserBlog)
-router.route('/edit/:id').post(editBlogText)
+router.route('/user/:id').get(protect, getUserBlog)
+router.route('/edit/:id').post(protect, editBlogText)
 router.route('/blogpost').post(protect, createBlogPost)
-router.route('/delete/:id').delete(deleteBlog)
+router.route('/delete/:id').delete(protect, deleteBlog)
 
 module.exports = router
